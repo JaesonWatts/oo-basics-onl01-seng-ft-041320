940be945fa5eaf7d531c51a2d083be721af4c41c
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-23 05:54:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,26 @@
 # Make your shoe class here!
 class Shoe
    
+<<<<<<< HEAD
   attr_accessor :brand, :color, :size, :material, :condition
 
+=======
+  attr_accessor 
+  attr_reader 
+ 
+>>>>>>> c75a4dae37ea4eedfd29b85ea4033b17a2e4f12c
   def initialize(brand)
     @brand = brand
   end
  
  
   def cobble
+<<<<<<< HEAD
     puts "Your shoe is as good as new!"
     @condition = "new"
+=======
+    puts ""
+>>>>>>> c75a4dae37ea4eedfd29b85ea4033b17a2e4f12c
   end
 
 end
